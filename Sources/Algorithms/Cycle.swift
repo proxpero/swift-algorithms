3057ff42eae95c748a2979cf387da2980ba50e1c
Todd Olsen <todd.olsen@me.com>
2020-10-08 00:02:03 -0400

    Re-indent. :shrug.

--- Sources/Algorithms/Cycle.swift
+++ Sources/Algorithms/Cycle.swift
@@ -11,31 +11,31 @@
 
 /// A collection wrapper that repeats the elements of a base collection.
 public struct Cycle<Base: Collection> {
-  /// The collection to repeat.
-  public let base: Base
+    /// The collection to repeat.
+    public let base: Base
 }
 
 extension Cycle: Sequence {
-  /// The iterator for a `Cycle` sequence.
-  public struct Iterator: IteratorProtocol {
-    let base: Base
-    var current: Base.Index
-    
-    public mutating func next() -> Base.Element? {
-      guard !base.isEmpty else { return nil }
-      
-      if current == base.endIndex {
-        current = base.startIndex
-      }
-      
-      defer { base.formIndex(after: &current) }
-      return base[current]
+    /// The iterator for a `Cycle` sequence.
+    public struct Iterator: IteratorProtocol {
+        let base: Base
+        var current: Base.Index
+        
+        public mutating func next() -> Base.Element? {
+            guard !base.isEmpty else { return nil }
+            
+            if current == base.endIndex {
+                current = base.startIndex
+            }
+            
+            defer { base.formIndex(after: &current) }
+            return base[current]
+        }
     }
-  }
-  
-  public func makeIterator() -> Iterator {
-    Iterator(base: base, current: base.startIndex)
-  }
+    
+    public func makeIterator() -> Iterator {
+        Iterator(base: base, current: base.startIndex)
+    }
 }
 
 //===----------------------------------------------------------------------===//
@@ -43,34 +43,34 @@ extension Cycle: Sequence {
 //===----------------------------------------------------------------------===//
 
 extension Collection {
-  /// Returns a sequence that repeats the elements of this collection forever.
-  ///
-  /// Use the `cycled()` method to repeat the elements of a sequence or
-  /// collection forever. You can combine `cycled()` with another, finite
-  /// sequence to iterate over the two together.
-  ///
-  ///     for (evenOrOdd, number) in zip(["even", "odd"].cycled(), 0..<10) {
-  ///         print("\(number) is \(evenOrOdd)")
-  ///     }
-  ///     // 0 is even
-  ///     // 1 is odd
-  ///     // 2 is even
-  ///     // 3 is odd
-  ///     // ...
-  ///     // 9 is odd
-  ///
-  /// - Important: When called on a non-empty collection, the resulting sequence
-  ///   is infinite. Do not directly call methods that require a finite
-  ///   sequence, like `map` or `filter`, without first constraining the length
-  ///   of the cycling sequence.
-  ///
-  /// - Returns: A sequence that repeats the elements of this collection
-  ///   forever.
-  ///
-  /// - Complexity: O(1)
-  public func cycled() -> Cycle<Self> {
-    Cycle(base: self)
-  }
+    /// Returns a sequence that repeats the elements of this collection forever.
+    ///
+    /// Use the `cycled()` method to repeat the elements of a sequence or
+    /// collection forever. You can combine `cycled()` with another, finite
+    /// sequence to iterate over the two together.
+    ///
+    ///     for (evenOrOdd, number) in zip(["even", "odd"].cycled(), 0..<10) {
+    ///         print("\(number) is \(evenOrOdd)")
+    ///     }
+    ///     // 0 is even
+    ///     // 1 is odd
+    ///     // 2 is even
+    ///     // 3 is odd
+    ///     // ...
+    ///     // 9 is odd
+    ///
+    /// - Important: When called on a non-empty collection, the resulting sequence
+    ///   is infinite. Do not directly call methods that require a finite
+    ///   sequence, like `map` or `filter`, without first constraining the length
+    ///   of the cycling sequence.
+    ///
+    /// - Returns: A sequence that repeats the elements of this collection
+    ///   forever.
+    ///
+    /// - Complexity: O(1)
+    public func cycled() -> Cycle<Self> {
+        Cycle(base: self)
+    }
 }
 
 //===----------------------------------------------------------------------===//
@@ -78,24 +78,24 @@ extension Collection {
 //===----------------------------------------------------------------------===//
 
 extension Collection {
-  /// Returns a sequence that repeats the elements of this collection the
-  /// specified number of times.
-  ///
-  /// Passing `1` as `times` results in this collection's elements being
-  /// provided a single time; passing `0` results in an empty sequence. The
-  /// `print(_:)` function in this example is never called:
-  ///
-  ///     for x in [1, 2, 3].cycled(times: 0) {
-  ///         print(x)
-  ///     }
-  ///
-  /// - Parameter times: The number of times to repeat this sequence. `times`
-  ///   must be zero or greater.
-  /// - Returns: A sequence that repeats the elements of this sequence `times`
-  ///   times.
-  ///
-  /// - Complexity: O(1)
-  public func cycled(times: Int) -> FlattenSequence<Repeated<Self>> {
-    repeatElement(self, count: times).joined()
-  }
+    /// Returns a sequence that repeats the elements of this collection the
+    /// specified number of times.
+    ///
+    /// Passing `1` as `times` results in this collection's elements being
+    /// provided a single time; passing `0` results in an empty sequence. The
+    /// `print(_:)` function in this example is never called:
+    ///
+    ///     for x in [1, 2, 3].cycled(times: 0) {
+    ///         print(x)
+    ///     }
+    ///
+    /// - Parameter times: The number of times to repeat this sequence. `times`
+    ///   must be zero or greater.
+    /// - Returns: A sequence that repeats the elements of this sequence `times`
+    ///   times.
+    ///
+    /// - Complexity: O(1)
+    public func cycled(times: Int) -> FlattenSequence<Repeated<Self>> {
+        repeatElement(self, count: times).joined()
+    }
 }
--- Sources/Algorithms/Unique.swift
+++ Sources/Algorithms/Unique.swift
@@ -14,34 +14,34 @@
 //===----------------------------------------------------------------------===//
 
 extension Sequence where Element: Hashable {
-  /// Returns an array with only the unique elements of this sequence, in the
-  /// order of the first occurence of each unique element.
-  public func uniqued() -> [Element] {
-    var seen: Set<Element> = []
-    var result: [Element] = []
-    for element in self {
-      if seen.insert(element).inserted {
-        result.append(element)
-      }
+    /// Returns an array with only the unique elements of this sequence, in the
+    /// order of the first occurence of each unique element.
+    public func uniqued() -> [Element] {
+        var seen: Set<Element> = []
+        var result: [Element] = []
+        for element in self {
+            if seen.insert(element).inserted {
+                result.append(element)
+            }
+        }
+        return result
     }
-    return result
-  }
 }
 
 extension Sequence {
-  /// Returns an array with the unique elements of this sequence (as determined
-  /// by the given projection), in the order of the first occurence of each
-  /// unique element.
-  public func uniqued<Subject: Hashable>(
-    on projection: (Element) throws -> Subject
-  ) rethrows -> [Element] {
-    var seen: Set<Subject> = []
-    var result: [Element] = []
-    for element in self {
-      if seen.insert(try projection(element)).inserted {
-        result.append(element)
-      }
+    /// Returns an array with the unique elements of this sequence (as determined
+    /// by the given projection), in the order of the first occurence of each
+    /// unique element.
+    public func uniqued<Subject: Hashable>(
+        on projection: (Element) throws -> Subject
+    ) rethrows -> [Element] {
+        var seen: Set<Subject> = []
+        var result: [Element] = []
+        for element in self {
+            if seen.insert(try projection(element)).inserted {
+                result.append(element)
+            }
+        }
+        return result
     }
-    return result
-  }
 }
